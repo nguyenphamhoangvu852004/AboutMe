debfde239223a803800680b92fcfc8b341726f91
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2024-08-03 00:52:59 +0700

    final commit

--- script.js
+++ script.js
@@ -11,7 +11,9 @@ document.addEventListener('DOMContentLoaded', function () {
         { href: "https://www.facebook.com/profile.php?id=61556666886780", iconClass: "bx bxl-facebook", delay: 1 },
         { href: "https://www.youtube.com/channel/UC3mgi8-1r5pMCBPiY_Xsq4A", iconClass: "bx bxl-youtube", delay: 2 },
         { href: "https://www.tiktok.com/", iconClass: "bx bxl-tiktok", delay: 3 },
-        { href: "https://github.com/nguyenphamhoangvu852004", iconClass: "bx bxl-github", delay: 4 }
+        { href: "https://github.com/nguyenphamhoangvu852004", iconClass: "bx bxl-github", delay: 4 },
+       
+
     ];
 
     const socialMediaContainer = document.getElementById('social-media');
